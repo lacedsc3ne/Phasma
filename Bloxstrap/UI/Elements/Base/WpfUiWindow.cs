5aa303086ada3d84264368bce5c3df5f90c65963
laced <support.phasma@gmail.com>
2026-08-28 20:34:54 +0200

    Rebrand as PhasmaStrap and match PhasmaMacro's visual style
    Rename all visible Bloxstrap branding to PhasmaStrap (project name,
    links, dialog/icon labels, ~72 UI strings), swap in the Phasma app
    icon, and apply PhasmaMacro's warm dark palette and coral accent
    color in place of the default Bloxstrap theme.

--- Bloxstrap/UI/Elements/Base/WpfUiWindow.cs
+++ Bloxstrap/UI/Elements/Base/WpfUiWindow.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Interop;
+using System.Windows.Media;
 using Wpf.Ui.Appearance;
 using Wpf.Ui.Controls;
 using Wpf.Ui.Mvvm.Contracts;
@@ -11,6 +12,9 @@ namespace Bloxstrap.UI.Elements.Base
     {
         private readonly IThemeService _themeService = new ThemeService();
 
+        // Phasma brand accent (coral-red), matches PhasmaMacro's --accent
+        private static readonly Color PhasmaAccent = Color.FromRgb(0xF4, 0x55, 0x4B);
+
         public WpfUiWindow()
         {
             ApplyTheme();
@@ -21,7 +25,7 @@ namespace Bloxstrap.UI.Elements.Base
             const int customThemeIndex = 2; // index for CustomTheme merged dictionary
 
             _themeService.SetTheme(App.Settings.Prop.Theme.GetFinal() == Enums.Theme.Dark ? ThemeType.Dark : ThemeType.Light);
-            _themeService.SetSystemAccent();
+            _themeService.SetAccent(PhasmaAccent);
 
             // there doesn't seem to be a way to query the name for merged dictionaries
             var dict = new ResourceDictionary { Source = new Uri($"pack://application:,,,/UI/Style/{Enum.GetName(App.Settings.Prop.Theme.GetFinal())}.xaml") };
